4372e821f983e20c660cc0f9a5737e634d39e05a
Przemek Dolowy <00758120@harper-adams.ac.uk>
2023-03-06 18:28:44 +0000

    libs added

--- code/functions/PackageCheck_F.R
+++ code/functions/PackageCheck_F.R
@@ -5,11 +5,11 @@
 # installs and loads required packages and returns a list of their names
 
 # https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
-PackageCheck<-function(libs) {
+PackageCheck<-function(libs, repos) {
   req<-unlist(lapply(libs,require,character.only=TRUE))
   need<-libs[req==FALSE]
   if(length(need)>0){ 
-    install.packages(need)
+    install.packages(need, repos = repos)
     lapply(need,require,character.only=TRUE)
   }
 }
